3012ae728c1b9759b897e99fd4402253b2b61075
alexrossoni <alexjuniorjn@hotmail.com>
2024-02-03 20:55:58 -0300

    Fix: Corrigindo title da página 404 Not Found

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,24 +2,30 @@ import { Container, ImageContainer } from "../styles/404-styles";
 import Image from "next/image";
 import pokeballIcon from "../../public/images/red-pokeball.svg";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function NotFoundPage() {
   const router = useRouter();
 
   return (
-    <Container>
-      <h1>Página não encontrada!</h1>
-      <ImageContainer>
-        <span className="questionIcon">?</span>
-        <Image
-          src={pokeballIcon}
-          alt="Ícone de uma pokébola"
-          width={150}
-          onClick={() => {
-            router.push("/");
-          }}
-        />
-      </ImageContainer>
-    </Container>
+    <>
+      <Head>
+        <title>Centro Pokémon - Página não encontrada</title>
+      </Head>
+      <Container>
+        <h1>Página não encontrada!</h1>
+        <ImageContainer>
+          <span className="questionIcon">?</span>
+          <Image
+            src={pokeballIcon}
+            alt="Ícone de uma pokébola"
+            width={150}
+            onClick={() => {
+              router.push("/");
+            }}
+          />
+        </ImageContainer>
+      </Container>
+    </>
   );
 }
